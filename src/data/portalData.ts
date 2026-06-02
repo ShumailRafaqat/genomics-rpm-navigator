1fc4d044177d6986954608090f0e2cfc347c6c2e
SHUMAIL RAFAQAT <shumailrafaqat2003@gmail.com>
2026-06-02 11:30:13 -0700

    Update portalData.ts

--- src/data/portalData.ts
+++ src/data/portalData.ts
@@ -113,7 +113,7 @@ export const mgsRpmPipeline: PipelineData = {
     { 
       title: "Elizabeth-bah-RPM", 
       icon: "file-text", 
-      url: "@/assets/benefits-pitch.mp3"", 
+      url: "/opening-script.mp3", 
       description: "Opening Greeting + Introduction",
       isAudio: true 
     },
