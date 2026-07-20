1f7601908258c1a74da618d7f3a6f3a6d38e0d9e
SHUMAIL RAFAQAT <shumailrafaqat2003@gmail.com>
2026-07-20 10:53:54 -0700

    Update portalData.ts

--- src/data/portalData.ts
+++ src/data/portalData.ts
@@ -187,7 +187,7 @@ export const trainingPipeline: PipelineData = {
     {
       title: "Training Video 1",
       icon: "video",
-      url: "",
+      url: "https://res.cloudinary.com/dyxql7jwr/video/upload/v1784569945/WhatsApp_Video_2026-07-20_at_10.30.26_AM_mmc5uw.mp4",
       isVideo: true
     },
     {
